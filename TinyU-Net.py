b147d8ea5ab3ae28ad244743a49dbe19e3475b97
陈俊任 <907409001@qq.com>
2024-08-20 17:18:32 +0800

    Create TinyU-Net.py

---
+++ TinyU-Net.py
@@ -0,0 +1,164 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import math
+from thop import clever_format, profile
+from torchsummary import summary
+
+def autopad(k, p=None, d=1):  
+    '''
+    k: kernel
+    p: padding
+    d: dilation
+    '''
+    if d > 1:
+        # actual kernel-size
+        k = d * (k - 1) + 1 if isinstance(k, int) else [d * (x - 1) + 1 for x in k] # actual kernel-size
+    if p is None:
+        # auto-pad
+        p = k // 2 if isinstance(k, int) else [x // 2 for x in k] # auto-pad
+    return p
+
+class Conv(nn.Module):
+    """Standard convolution with args(ch_in, ch_out, kernel, stride, paddin g, groups, dilation, activation)."""
+    default_act = nn.GELU()
+    def __init__(self, c1, c2, k=1, s=1, p=None, g=1, d=1, act=True):
+        super().__init__()
+        self.conv   = nn.Conv2d(c1, c2, k, s, autopad(k, p, d), groups=g, dilation=d, bias=False)
+        self.bn     = nn.BatchNorm2d(c2, eps=0.001, momentum=0.03, affine=True, track_running_stats=True)
+        self.act    = self.default_act if act is True else act if isinstance(act, nn.Module) else nn.Identity()
+
+    def forward(self, x):
+        return self.act(self.bn(self.conv(x)))
+
+    def forward_fuse(self, x):
+        return self.act(self.conv(x))
+    
+class DWConv(Conv):
+    """Depth-wise convolution."""
+
+    def __init__(self, c1, c2, k=1, s=1, d=1, act=True):  # ch_in, ch_out, kernel, stride, dilation, activation
+        super().__init__(c1, c2, k, s, g=math.gcd(c1, c2), d=d, act=act)
+
+    
+# Lightweight Cascade Multi-Receptive Fields Module
+class CMRF(nn.Module):
+    """Implementation of CMRF Module."""
+    def __init__(self, c1, c2, n=7, shortcut=True, g=1, e=0.5):  # ch_in, ch_out, number, shortcut, groups, expansion
+        super().__init__()
+        
+        self.n = n
+        self.all_group = n+1
+    
+        self.c = int(c2 * e / self.all_group)  # hidden channels
+        # assert  self.all_group%2 == 0, "self.c should be divided by self.all_group."
+        
+        
+        self.pwconv1 = Conv(c1, c2//self.all_group, 1, 1)
+        
+        self.pwconv2 = Conv(c2//2, c2, 1, 1)
+        
+        self.m = nn.ModuleList(DWConv(self.c, self.c, k=3, act=False) for _ in range(n))
+
+        self.add = shortcut  and c1 == c2
+        
+        
+    def forward(self, x):
+        """Forward pass through CMRF Module."""
+        x_residual = x
+        x = self.pwconv1(x)
+
+        x = [x[:, 0::2, :, :], x[:, 1::2, :, :]]
+        x.extend(m(x[-1]) for m in self.m)
+        x[0] = x[0] +  x[1] 
+        x.pop(1)
+        
+        c = torch.cat(x, dim=1) 
+        c = self.pwconv2(c)
+        
+        return x_residual + c if self.add else c
+
+
+
+
+
+'''
+U-shape/U-like Model
+'''
+# Encoder in TinyU-Net
+class UNetEncoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetEncoder, self).__init__()
+        self.cmrf       = CMRF(in_channels, out_channels)
+        self.downsample = nn.MaxPool2d(kernel_size=2, stride=2)
+        
+    def forward(self, x):
+        x               = self.cmrf(x)
+
+        return self.downsample(x), x
+    
+
+# Decoder in TinyU-Net
+class UNetDecoder(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNetDecoder, self).__init__()
+        self.cmrf     = CMRF(in_channels, out_channels)
+        self.upsample = F.interpolate
+        
+    def forward(self, x, skip_connection):
+        x = self.upsample(x, scale_factor=2, mode='bicubic', align_corners=False)
+        x = torch.cat([x, skip_connection], dim=1)
+        x = self.cmrf(x)
+
+        return x
+    
+
+# TinyU-Net
+class TinyUNet(nn.Module):
+    def __init__(self, in_channels=3, num_classes=2):
+        super(TinyUNet, self).__init__()
+        in_filters  = [192, 384, 768, 1024]
+        out_filters = [64, 128, 256, 512]
+        
+        self.encoder1   = UNetEncoder(in_channels, 64)
+        self.encoder2   = UNetEncoder(64, 128)
+        self.encoder3   = UNetEncoder(128, 256)
+        self.encoder4   = UNetEncoder(256, 512)
+
+        self.decoder4   = UNetDecoder(in_filters[3], out_filters[3])
+        self.decoder3   = UNetDecoder(in_filters[2], out_filters[2])
+        self.decoder2   = UNetDecoder(in_filters[1], out_filters[1])
+        self.decoder1   = UNetDecoder(in_filters[0], out_filters[0])
+        self.final_conv = nn.Conv2d(out_filters[0], num_classes, kernel_size=1)
+        
+    def forward(self, x):
+        x, skip1 = self.encoder1(x)
+        x, skip2 = self.encoder2(x)
+        x, skip3 = self.encoder3(x)
+        x, skip4 = self.encoder4(x)
+
+        x = self.decoder4(x, skip4)
+        x = self.decoder3(x, skip3)
+        x = self.decoder2(x, skip2)
+        x = self.decoder1(x, skip1)
+        x = self.final_conv(x)
+        return x
+
+
+if __name__ == '__main__':
+    tinyunet = TinyUNet(in_channels=3, num_classes=2)
+
+    device   = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    tinyunet = tinyunet.to(device)
+
+    # summary(tinyunet, (3, 256, 256))
+        
+    dummy_input     = torch.randn(1, 3, 256, 256).to(device)
+    flops, params   = profile(tinyunet, (dummy_input, ), verbose=False)
+    #-------------------------------------------------------------------------------#
+    #   flops * 2 because profile does not consider convolution as two operations.
+    #-------------------------------------------------------------------------------#
+    flops           = flops * 2
+    flops, params   = clever_format([flops, params], "%.4f")
+    print(f'Total GFLOPS: {flops}')
+    print(f'Total Params: {params}')
